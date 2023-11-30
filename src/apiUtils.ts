b9ebf4f0da6f90173d3a6031831d97abd96373a3
Kirah Sapong <kirah@tbd.email>
2023-11-29 17:33:56 -0800

    Create methods for checking and adding tbdollars to account

--- src/apiUtils.ts
+++ src/apiUtils.ts
@@ -1,7 +1,56 @@
+import { PortableDid } from '@web5/dids'
 import { fromCents } from './currency-utils'
 import { TbdexHttpClient, Rfq, Message, SelectedPaymentMethod } from '@tbdex/http-client'
+import { JwtHeader, JwtPayload } from 'jwt-decode'
+import { Convert } from '@web5/common'
+import { Ed25519, Jose } from '@web5/crypto'
 
-const samplePfiDid = 'did:dht:qddijzptgwczufcj7ut6xcxmbunpg1bx43zu9xaorjpsb7xmmjxo'
+const samplePfiDid = 'did:dht:rriy6zgrhh9c53kbbifkamhmfi4y8ep8sibxsri9hj9q8wx6f1ao'
+
+
+export async function checkTbdDollars(didState) {
+  return await createTBDollarsRequest(didState, '/tbdollars?check=true')
+}
+
+export async function addTBDollars(didState) {
+  return await createTBDollarsRequest(didState, '/tbdollars')
+}
+
+async function createTBDollarsRequest(didState, reqEndpoint) {
+  try {
+    const pfiEndpoint = await TbdexHttpClient.getPfiServiceEndpoint(samplePfiDid)
+    const token = await createJwtToken(didState)
+    const res = await fetch(pfiEndpoint + reqEndpoint, {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    return await res.json()
+  } catch (e) {
+    throw new Error(e)
+  }
+}
+
+async function createJwtToken(did: PortableDid) {
+  const privateKeyJwk = did.keySet.verificationMethodKeys[0].privateKeyJwk
+
+  const header: JwtHeader = { typ: 'JWT', alg: privateKeyJwk.alg, kid: did.document.verificationMethod[0].id }
+  const base64UrlEncodedHeader = Convert.object(header).toBase64Url()
+
+  const payload: JwtPayload = { jti: new Date().toISOString() }
+  const base64UrlEncodedPayload = Convert.object(payload).toBase64Url()
+
+  const toSign = `${base64UrlEncodedHeader}.${base64UrlEncodedPayload}`
+  const toSignBytes = Convert.string(toSign).toUint8Array()
+
+  const { keyMaterial } = await Jose.jwkToKey({ key: privateKeyJwk })
+
+  const signatureBytes = await Ed25519.sign({ key: keyMaterial, data: toSignBytes })
+  const base64UrlEncodedSignature = Convert.uint8Array(signatureBytes).toBase64Url()
+
+  return `${toSign}.${base64UrlEncodedSignature}`
+}
+
 
 export async function fetchOfferings() {
   const response = await TbdexHttpClient.getOfferings({
@@ -57,6 +106,7 @@ export async function fetchExchanges(didState) {
     pfiDid: samplePfiDid,
     ...getDidDetails(didState)
   })
+  console.log(response.data)
   const exchanges = response.data.map(exchange => {
     const latestMessage = exchange[exchange.length - 1]
     const status = generateExchangeStatusValues(latestMessage.metadata.kind)
@@ -84,7 +134,7 @@ export type ClientExchange = {
   expirationTime?: string
 }
 
-function getDidDetails(didState) {
+function getDidDetails(didState: PortableDid) {
   return {
     privateKeyJwk: didState.keySet.verificationMethodKeys.find(key => key.relationships.includes('authentication')).privateKeyJwk,
     kid: didState.document.verificationMethod.find(method => method.controller === didState.did).id
