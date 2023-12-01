5f45f7bf896196fbae41a0ecaf184c70ae0b4818
kirahsapong <102400653+kirahsapong@users.noreply.github.com>
2023-11-30 19:19:29 -0800

    Feat/add blank functions (#28)
    * Clean up activity page and add balance and credentials components
    
    * Clean up rfq modal pages
    
    * Clean up exchanges
    
    * Add blank functions and answer key
    
    * Add placeholders for code
    
    * Drop console.log
    
    * Rearrange functions in empty template code file
    
    * Uncomment answer key

---
+++ src/answerKey/messageUtils.ts
@@ -0,0 +1,96 @@
+import { Order, Rfq, SelectedPaymentMethod, TbdexHttpClient } from '@tbdex/http-client'
+import { PortableDid } from '@web5/dids'
+
+export type SendRfqOptions = {
+  offeringId: string, 
+  payinSubunits: string, 
+  payinMethod: SelectedPaymentMethod,
+  payoutMethod: SelectedPaymentMethod,
+  credentials: string[],
+  didState: PortableDid,
+  pfiDid: string
+}
+
+export type SendOrderOptions = {
+  exchangeId: string,
+  didState: PortableDid,
+  pfiDid: string
+}
+
+export async function sendRFQ(opts: SendRfqOptions) {
+  const { 
+    offeringId, 
+    payinSubunits, 
+    payinMethod,
+    payoutMethod,
+    credentials,
+    didState,
+    pfiDid
+  } = opts
+  const message = Rfq.create({
+    data: {
+      offeringId,
+      payinSubunits,
+      payinMethod,
+      payoutMethod,
+      claims: credentials
+    },
+    metadata: {
+      from: didState.did,
+      to: pfiDid
+    }
+  })
+  await message.sign(didState)
+  return await TbdexHttpClient.sendMessage({ message })
+}
+
+export async function sendOrder(opts: SendOrderOptions) {
+  const { 
+    exchangeId,
+    didState,
+    pfiDid
+  } = opts
+  const message = Order.create({
+    metadata: {
+      exchangeId: exchangeId,
+      from: didState.did,
+      to: pfiDid
+    }
+  })
+  await message.sign(didState)
+  return await TbdexHttpClient.sendMessage({ message })
+}
+
+export function generateExchangeStatusValues(exchange) {
+  if (exchange.kind === 'close') {
+    if (exchange.data.reason.toLowerCase().includes('completed')) {
+      return 'completed'
+    } else if (exchange.data.reason.toLowerCase().includes('expired')) {
+      return 'expired'
+    } else {
+      return 'failed'
+    }
+  }
+  return exchange.kind
+}
+
+export function renderOrderStatus (exchange) {
+  const status = generateExchangeStatusValues(exchange)
+  switch (status) {
+    case 'rfq':
+      return 'Requested'
+    case 'quote':
+      return 'Quoted'
+    case 'order':
+    case 'orderstatus':
+      return 'Pending'
+    case 'completed':
+      return 'Completed'
+    case 'expired':
+      return 'Expired'
+    case 'failed':
+      return 'Failed'
+    default:
+      return 'Unknown status'
+  }
+}
